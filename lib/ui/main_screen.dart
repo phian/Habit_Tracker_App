68f34a9c57b379a9d0bf91cb62820e466eb329b7
Ân Nguyễn Phi <17520217@ms.uit.edu.vn>
2020-10-29 21:59:10 +0700

    Remove default code and import set up code

---
+++ lib/ui/main_screen.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class MainScreen extends StatefulWidget {
+  @override
+  _MainScreenState createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: Center(
+          child: Text(
+            "This is Habit Tracking App main screen",
+            style: TextStyle(
+              fontSize: 30.0,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}

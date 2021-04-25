e0b1230921d8bec0d0665f100ea85300585d7d81
Asus <17520217@gm.uit.edu,vn>
2021-04-25 19:08:33 +0700

    Update code to same with master

--- lib/constants/app_constant.dart
+++ lib/constants/app_constant.dart
@@ -1,22 +1,4 @@
 import 'package:flutter/cupertino.dart';
-import 'package:get/get.dart';
-import 'package:habit_tracker/main.dart';
-import 'package:habit_tracker/view/all_habits_screen.dart';
-import 'package:habit_tracker/view/challenges_screen.dart';
-import 'package:habit_tracker/view/create_and_edit_habit_screen/create_habit_screen.dart';
-import 'package:habit_tracker/view/create_and_edit_habit_screen/edit_habit_screen.dart';
-import 'package:habit_tracker/view/general_screen/genaral_screeen.dart';
-import 'package:habit_tracker/view/habit_all_note_screen/habit_all_note_screen.dart';
-import 'package:habit_tracker/view/habit_categories_screen/habit_categories_screen.dart';
-import 'package:habit_tracker/view/habit_category_list_screen/habit_category_list_screen.dart';
-import 'package:habit_tracker/view/habit_note_screen.dart';
-import 'package:habit_tracker/view/habit_statistic_screen.dart';
-import 'package:habit_tracker/view/login_screen.dart';
-import 'package:habit_tracker/view/main_screen.dart';
-import 'package:habit_tracker/view/manage_screen.dart';
-import 'package:habit_tracker/view/notification_screen.dart';
-import 'package:habit_tracker/view/step_tracking_screen.dart';
-import 'package:habit_tracker/widgets/timer/timer.dart';
 import 'package:shrink_sidemenu/shrink_sidemenu.dart';
 
 class AppConstant {
@@ -35,34 +17,6 @@ class AppConstant {
       GlobalKey<SideMenuState>(
     debugLabel: "StepTrackingScreenKey",
   );
-
-  static final List<GetPage> listPage = [
-    GetPage(name: '/splash_screen', page: () => IntroScreen()),
-    // sửa thành splash screen
-    GetPage(name: '/manage_screen', page: () => ManageScreen()),
-    //
-    GetPage(name: '/main', page: () => MainScreen()),
-    // nên đổi tên thành home
-    GetPage(name: '/all_habit', page: () => AllHabitsScreen()),
-    GetPage(name: '/challenge', page: () => ChallengesScreen()),
-    GetPage(name: '/step_tracking', page: () => StepTackingScreen()),
-    //
-    GetPage(name: '/statistic', page: () => HabitStatisticScreen()),
-    GetPage(name: '/note', page: () => HabitNoteScreen()),
-    GetPage(name: '/all_note', page: () => HabitAllNoteScreen()),
-    //
-    GetPage(name: '/create_habit', page: () => CreateHabitScreen()),
-    GetPage(name: '/edit_habit', page: () => EditHabitScreen()),
-    //
-    GetPage(name: '/suggest_category', page: () => HabitCategoriesScreen()),
-    GetPage(
-        name: '/suggest_category_list', page: () => HabitCategoryListScreen()),
-    //
-    GetPage(name: '/notification', page: () => NotificationScreen()),
-    GetPage(name: '/general', page: () => GeneralScreen()),
-    GetPage(name: '/login', page: () => LoginScreen()),
-    GetPage(name: '/timer', page: () => Timer())
-  ];
 }
 
 enum AllNoteLoadingState {
---
+++ lib/routing/pages.dart
@@ -0,0 +1,51 @@
+import 'package:get/get_navigation/src/routes/get_route.dart';
+import 'package:habit_tracker/widgets/timer/timer.dart';
+
+import '../main.dart';
+import '../routing/routes.dart';
+import '../view/all_habits_screen.dart';
+import '../view/challenges_screen.dart';
+import '../view/create_and_edit_habit_screen/create_habit_screen.dart';
+import '../view/create_and_edit_habit_screen/edit_habit_screen.dart';
+import '../view/general_screen/genaral_screeen.dart';
+import '../view/habit_all_note_screen/habit_all_note_screen.dart';
+import '../view/habit_categories_screen/habit_categories_screen.dart';
+import '../view/habit_category_list_screen/habit_category_list_screen.dart';
+import '../view/habit_note_screen.dart';
+import '../view/habit_statistic_screen.dart';
+import '../view/login_screen.dart';
+import '../view/main_screen.dart';
+import '../view/manage_screen.dart';
+import '../view/notification_screen.dart';
+import '../view/step_tracking_screen.dart';
+
+class Pages {
+  static final List<GetPage> pages = [
+    GetPage(name: Routes.SPLASH_SCREEN, page: () => IntroScreen()),
+    // sửa thành splash screen
+    GetPage(name: Routes.MANAGE_SCRREN, page: () => ManageScreen()),
+    //
+    GetPage(name: Routes.MAIN_SCRREEN, page: () => MainScreen()),
+    GetPage(name: Routes.ALL_HABIT, page: () => AllHabitsScreen()),
+    GetPage(name: Routes.CHALLENGE, page: () => ChallengesScreen()),
+    GetPage(name: Routes.STEP_TRACKING, page: () => StepTackingScreen()),
+    //
+    GetPage(name: Routes.STATISTIC, page: () => HabitStatisticScreen()),
+    GetPage(name: Routes.NOTE, page: () => HabitNoteScreen()),
+    GetPage(name: Routes.ALL_NOTE, page: () => HabitAllNoteScreen()),
+    //
+    GetPage(name: Routes.CREATE_HABIT, page: () => CreateHabitScreen()),
+    GetPage(name: Routes.EDIT_HABIT, page: () => EditHabitScreen()),
+    //
+    GetPage(name: Routes.SUGGEST_CATEGORY, page: () => HabitCategoriesScreen()),
+    GetPage(
+        name: Routes.SUGGEST_CATEGORY_LIST,
+        page: () => HabitCategoryListScreen()),
+    //
+    GetPage(name: Routes.NOTIFICATION, page: () => NotificationScreen()),
+    GetPage(name: Routes.GENERAL, page: () => GeneralScreen()),
+    GetPage(name: Routes.LOGIN, page: () => LoginScreen()),
+    //
+    GetPage(name: Routes.TIMER, page: () => Timer()),
+  ];
+}
